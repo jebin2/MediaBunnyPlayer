8e7a36066bb1092d8f82b2a9998940b3bc80e003
Jebin Einstein <jebineinstein@gmail.com>
2025-10-25 23:50:41 +0530

    added minimal player support

--- js/main.js
+++ js/main.js
@@ -1,39 +1,17 @@
-import {setupEventListeners } from './eventListeners.js';
-import { renderLoop } from './player.js';
-import { updatePlaylistUIOptimized } from './playlist.js';
-import { dynamicVideoUrl, registerServiceWorker} from './utility.js';
-import { resize_define } from './resize.js';
-import { setupImageToVideo } from './imageToVideo.js';
-import { setupRecordingListeners } from './recording.js';
-
-import { canEncodeAudio } from 'https://cdn.jsdelivr.net/npm/mediabunny@1.24.0/+esm';
-import { registerMp3Encoder } from 'https://cdn.jsdelivr.net/npm/@mediabunny/mp3-encoder@1.24.0/+esm';
+import { allow_minimal } from './minimal_player.js';
+import { full_player } from './full_player.js';
 
 const initialize = async () => {
-	// As per the documentation, check if the browser can encode MP3 natively.
-	// If not, register the custom WASM-based encoder.
-	try {
-		if (!(await canEncodeAudio('mp3'))) {
-			console.log('Native MP3 encoder not found. Registering custom WASM encoder.');
-			await registerMp3Encoder();
-			console.log('Custom MP3 encoder registered successfully.');
-		} else {
-			console.log('Native MP3 encoder found.');
-		}
-	} catch (err) {
-		console.error('Failed to register the MP3 encoder:', err);
+	const urlParams = new URLSearchParams(window.location.search);
+	const onlyVideoPlayer = urlParams.get('only_video_player');
+	if (onlyVideoPlayer === 'true') {
+		allow_minimal();
+	} else {
+		document.querySelector(".entryloading").classList.add('hidden');
+		full_player()
 	}
-
-	setupEventListeners();
-	renderLoop();
-	dynamicVideoUrl();
-	updatePlaylistUIOptimized();
-	registerServiceWorker();
-	resize_define();
-    setupImageToVideo();
-	setupRecordingListeners();
 }
 
 // document.addEventListener('DOMContentLoaded', {
-	initialize();
+initialize();
 // });
