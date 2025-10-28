391675446e4975c4d64150355281e790f07bf4ee
Jebin Einstein <jebineinstein@gmail.com>
2025-10-28 08:35:25 +0530

    cache issue fix

--- service-worker.js
+++ service-worker.js
@@ -2,33 +2,66 @@ const CACHE_NAME = 'mediabunny-player-v1';
 
 const urlsToCache = [
     '/', // Only if served from root; adjust if deployed in subdirectory
+
+    //html
     '/index.html',
     '/player.html',
+
+    // json
     '/manifest.json',
+
+    // css
     '/css/style.css',
-    '/js/main.js',
-    '/js/player.js',
+    '/css/main1.css',
+
+    // js
+    '/js/audio.js',
+    '/js/blur.js',
+    '/js/caption.js',
     '/js/constants.js',
+    '/js/crop.js',
+    '/js/editing.js',
+    '/js/eventListeners.js',
+    '/js/imageToVideo.js',
+    '/js/main.js',
+    '/js/merge.js',
+    '/js/metadata.js',
+    '/js/minimal_player.js',
+    '/js/player.js',
+    '/js/playlist.js',
+    '/js/recording.js',
+    '/js/resize.js',
+    '/js/screenshot.js',
+    '/js/settings.js',
     '/js/state.js',
     '/js/ui.js',
-    '/js/audio.js',
-    '/js/settings.js',
-    '/js/eventListeners.js',
+    '/js/utility.js',
+
+    // icon
     '/favicon.ico',
+    '/favicon.png',
     '/icons/icon-192x192.svg',
-    '/icons/icon-512x512.svg'
+    '/icons/icon-512x512.svg',
+    '/media/dynamic_crop.gif',
+    '/upload.svg',
 ];
 
 // Install - pre-cache core assets
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('[Service Worker] Caching app shell');
-                return cache.addAll(urlsToCache);
-            })
-            .then(() => self.skipWaiting())
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(async cache => {
+      console.log('[Service Worker] Caching app shell');
+      for (const url of urlsToCache) {
+        try {
+          const response = await fetch(url);
+          if (!response.ok) throw new Error(`HTTP ${response.status}`);
+          await cache.put(url, response);
+        } catch (err) {
+          console.warn(`[SW] Failed to cache ${url}:`, err.message);
+        }
+      }
+    }).then(() => self.skipWaiting())
+  );
 });
 
 // Activate - clean up old caches
